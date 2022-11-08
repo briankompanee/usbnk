b681c7f96ad9ecaecb7cfaf77cb252dbfc5d8077
Brian Brown <brian@thekompanee.com>
2022-11-08 12:58:08 -0600

    Import to add main.css. Added <header>, <main>, and <footer> HTML5 Region elements instead of landmarks ex: (role=main)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
+import './styles/main.scss'
 import USBlogo from './images/usb-logo.svg';
+
 export default function App() {
   return (
-    <>
-      <div className="logo-container">
-        <img src={USBlogo} alt="U.S. Bank" />
-      </div>
-      <div>
-        {/* Add and debug Customer component */}
-        {/* Add filtered bank employees do display only bankers */}
-      </div>
-      <div>
-        <p>&copy; 2021 U.S.Bank</p>
+    <> 
+      <div className="App">
+        <header>
+          <div className="logo-container">
+            <img src={USBlogo} alt="U.S. Bank" />
+          </div>
+        </header>
+        <main>
+          {/* Add and debug Customer component */}
+          {/* Add filtered bank employees do display only bankers */}
+        </main>
+        <footer>
+          <p>&copy; 2021 U.S.Bank</p>
+        </footer>
       </div>
     </>
   );
