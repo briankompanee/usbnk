9ec60eb82f9c635444fc907c6b9a2de7e03afb3f
Brian Brown <brian@thekompanee.com>
2022-11-08 12:29:12 -0600

    imported logo from images directory and added image src to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import USBlogo from './images/usb-logo.svg';
 export default function App() {
   return (
     <>
-      <div className="logo-container"></div>
+      <div className="logo-container">
+        <img src={USBlogo} alt="U.S. Bank" />
+      </div>
       <div>
         {/* Add and debug Customer component */}
         {/* Add filtered bank employees do display only bankers */}
