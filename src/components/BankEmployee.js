35be783fba47fd542b332ad7bdd17c0f137d9c18
Brian Brown <brian@thekompanee.com>
2022-11-09 00:20:28 -0600

    Pass props.children as h2 header

--- src/components/BankEmployee.js
+++ src/components/BankEmployee.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const BankEmployee = (props) => { 
-  const { bankers } = props;
+  const { bankers, children } = props;
   return (
     <>
-      {bankers.map(banker => (
-        <div key={banker.id} className="bank-employee-info">
-          <div>{banker.userName}</div>
-          <div>Name: {banker.name}</div>
-          <div>Customer Satisfaction Rating: {banker.satisfactionRating}</div>
+      <h2>{ children }</h2>
+      { bankers.map(banker => (
+        <div key={ banker.id } className="bank-employee-info">
+          <div>{ banker.userName }</div>
+          <div>Name: { banker.name }</div>
+          <div>Customer Satisfaction Rating: { banker.satisfactionRating }</div>
         </div>
-      ))}
+      )) }
     </>
   );
 }
