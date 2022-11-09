46d89cfe81e0094cd91d935b5b0965ba60b0d052
Brian Brown <brian@thekompanee.com>
2022-11-09 00:31:28 -0600

    import banker data JSON, add banker comp and props.children to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import BankEmployee from './components/BankEmployee';
 import Customer from './components/Customer';
 
 import USBlogo from './images/usb-logo.svg';
 import './styles/main.scss';
 
+/* Using create-react-app the json-loader plugin is installed by default
+Import the JSON file */
+import userData from "./data/employees.json";
+
 export default function App() {
+  const bankers = [...userData]
+
   return (
     <> 
       <div className="App">
@@ -22,6 +29,11 @@ export default function App() {
               Customer
             </Customer>
           {/* Add filtered bank employees do display only bankers */}
+          <BankEmployee
+           bankers={ bankers }
+          > 
+          Employees
+          </BankEmployee>
         </main>
         <footer>
           <p>&copy; 2021 U.S.Bank</p>
